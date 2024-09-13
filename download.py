fbe68fa34d6cc24afb55bd7310318274deb0bd9d
UnknownPgr <unknownpgr@gmail.com>
2024-09-14 00:29:46 +0900

    Change data type from full data to core data

--- download.py
+++ download.py
@@ -11,6 +11,9 @@ from datasets import load_dataset
 
 dataset = load_dataset(
     "McAuley-Lab/Amazon-Reviews-2023",
-    "raw_review_Home_and_Kitchen",
+    "0core_timestamp_Home_and_Kitchen",
     trust_remote_code=True,
 )
+
+# List 10 examples
+print(dataset["train"][0:10])
